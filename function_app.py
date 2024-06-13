a1676e5b4de52a62ed41105f2de11863db1a8dde
Suah-Cho <sacho0929@gmail.com>
2024-06-13 17:46:30 +0900

    msg

--- function_app.py
+++ function_app.py
@@ -1,8 +1,20 @@
 import logging
 import azure.functions as func
+import os
+
+storage_connection_string = os.environ["CONNECT_STRING"]
+
+ssh_host = os.environ["SSH_HOST"]
+ssh_username = os.environ["SSH_USERNAME"]
+ssh_key = os.environ["SSH_KEY"]
+db_host = os.environ["DB_HOST"]
+db_name = os.environ["DB_NAME"]
+db_user = os.environ["DB_USER"]
+db_password = os.environ["DB_PASSWORD"]
 
 app = func.FunctionApp()
 
+logging.info("-----------------Python timer trigger function-----------------")
 @app.schedule(schedule="*/30 * * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
