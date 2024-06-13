bd523ea0928f991faf12a72d874e63bc770f6500
Suah-Cho <sacho0929@gmail.com>
2024-06-13 15:59:32 +0900

    init

--- function_app.py
+++ function_app.py
@@ -6,8 +6,6 @@ app = func.FunctionApp()
 @app.schedule(schedule="0 */1 * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function executed.')
     logging.error('This is an error message.')
