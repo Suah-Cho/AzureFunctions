e801c53cfefecc2dfad5ef2474a04711d1b428f6
Suah-Cho <sacho0929@gmail.com>
2024-06-13 16:33:22 +0900

    add env

--- function_app.py
+++ function_app.py
@@ -2,6 +2,14 @@ import logging
 import azure.functions as func
 import os
 
+ssh_host = os.environ["SSH_HOST"]
+ssh_username = os.environ["SSH_USERNAME"]
+ssh_key = os.environ["SSH_KEY"]
+db_host = os.environ["DB_HOST"]
+db_name = os.environ["DB_NAME"]
+db_user = os.environ["DB_USER"]
+db_password = os.environ["DB_PASSWORD"]
+
 app = func.FunctionApp()
 
 @app.schedule(schedule="0 */1 * * * *", arg_name="myTimer", run_on_startup=True,
