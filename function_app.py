cc4f9898f0244ed5b739a14603ee958699ccd965
Suah-Cho <sacho0929@gmail.com>
2024-06-13 16:20:24 +0900

    os test

--- function_app.py
+++ function_app.py
@@ -1,8 +1,8 @@
 import logging
 import azure.functions as func
 import os
-import sshtunnel
-import psycopg
+# import sshtunnel
+# import psycopg
 
 app = func.FunctionApp()
 
@@ -20,27 +20,5 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
 
     logging.info('Python timer trigger function executed.')
     logging.error('!!This is an error message!!')
-
-    try:
-        # Connect to the database
-        tunnel = sshtunnel.SSHTunnelForwarder(
-                (ssh_host, 22),
-                ssh_username=ssh_username,
-                ssh_pkey=ssh_key,
-                remote_bind_address=(db_host, 5432)
-            )
-
-        tunnel.start()
-
-        con = psycopg.connect(
-            host='localhost',
-            port=tunnel.local_bind_port,
-            database=db_name,
-            user=db_user,
-            password=db_password
-        )
-
-        logging.info("Connection established")
-    except Exception as e:
-        # Failed to connect to the database
-        logging.error(f"Database Connections Failed!!!: {e}")
+    logging.info(f"sshtunneling from {SSH_HOST}:22 to {DB_HOST}:5432")
+    
