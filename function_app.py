e4b0c729fb86d3ddb9afcf8c2e26297a8ccc9411
Suah-Cho <sacho0929@gmail.com>
2024-06-14 08:55:25 +0900

    add env

--- function_app.py
+++ function_app.py
@@ -1,13 +1,17 @@
 import logging
 import azure.functions as func
 import psycopg2
+import os
 
 app = func.FunctionApp()
 
 @app.schedule(schedule="*/30 * * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
-        logging.info('!!!!!!The timer is past due!!!!!!!')
 
     logging.info('!!!!!!!!!Python timer trigger function executed.!!!!!!!!')
+    logging.error('!!!!!add env!!!!!')
+
+    SSH_HOST = os.environ['SSH_HOST']
+    SSH_USERNAME = os.environ['SSH_USERNAME']
+    logging.info(f"SSH_HOST: {SSH_HOST}, SSH_USERNAME: {SSH_USERNAME}")
