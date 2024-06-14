a3577152d8ae6e5dff21952cae9c5162174f0b0d
Suah-Cho <sacho0929@gmail.com>
2024-06-14 10:41:19 +0900

    try to connect db

--- function_app.py
+++ function_app.py
@@ -20,16 +20,16 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
     logging.error('!!!!I WANNA SEE DB_CONNECTION_STRING!!!!')
 
     # connect to azure database
-    # try:
-    #     con = psycopg2.connect(db_connection_string)
-    #     cur = con.cursor()
-    #     logging.info("Connected to database")
+    try:
+        con = psycopg2.connect(db_connection_string)
+        cur = con.cursor()
+        logging.info("Connected to database")
 
-    #     cur.execute("SELECT * FROM gh_data_item limit 3;")
-    #     rows = cur.fetchall()
-    #     for row in rows:
-    #         logging.info(row)
-    #     cur.close()
-    #     con.close()
-    # except Exception as e:
-    #     logging.error(f"DB CONNECT Error: {e}")
+        cur.execute("SELECT * FROM gh_data_item limit 3;")
+        rows = cur.fetchall()
+        for row in rows:
+            logging.info(row)
+        cur.close()
+        con.close()
+    except Exception as e:
+        logging.error(f"DB CONNECT Error: {e}")
