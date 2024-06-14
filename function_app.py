37afff8fc5769e732b1e2690dac5af2620930a03
Suah-Cho <sacho0929@gmail.com>
2024-06-14 11:31:03 +0900

    add StringIO, pandas

--- function_app.py
+++ function_app.py
@@ -4,6 +4,8 @@ import psycopg2
 import sshtunnel
 import os
 from azure.storage.blob import BlobServiceClient
+from io import StringIO
+import pandas as pd
 
 app = func.FunctionApp()
 
@@ -11,17 +13,17 @@ def getCon():
     current_dir = os.path.dirname(os.path.realpath(__file__))
     ssh_key_path = os.path.join(current_dir, 'ssh_key.pem')
 
-    logging.info(f"ssh_key_path: {ssh_key_path}")
+    # logging.info(f"ssh_key_path: {ssh_key_path}")
 
     SSH_HOST = os.environ['SSH_HOST']
     SSH_USERNAME = os.environ['SSH_USERNAME']
-    SSH_KEY = os.environ['SSH_KEY']
-    logging.info(f"SSH_HOST: {SSH_HOST}, SSH_USERNAME: {SSH_USERNAME}")
+
+    # logging.info(f"SSH_HOST: {SSH_HOST}, SSH_USERNAME: {SSH_USERNAME}")
     DB_HOST = os.environ['DB_HOST']
     DB_USER = os.environ['DB_USER']
     DB_NAME = os.environ['DB_NAME']
     DB_PASSWORD = os.environ['DB_PASSWORD']
-    logging.info(f'DB_HOST: {DB_HOST}, DB_USER: {DB_USER}, DB_NAME: {DB_NAME}, DB_PASSWORD: {DB_PASSWORD}')
+    # logging.info(f'DB_HOST: {DB_HOST}, DB_USER: {DB_USER}, DB_NAME: {DB_NAME}, DB_PASSWORD: {DB_PASSWORD}')
     
     tunnel = sshtunnel.SSHTunnelForwarder(
         (SSH_HOST, 22),
@@ -41,6 +43,14 @@ def getCon():
     )
 
     return con
+
+
+def process_blob(blob, blob_service_client, container_client, con):
+    blob_client = container_client.get_blob_client(blob.name)
+    blob_content = StringIO(blob_client.download_blob().readall().decode('utf-8'))
+    # df = pd.read_csv(blob_content)
+
+
 
 @app.schedule(schedule="*/10 * * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
@@ -69,11 +79,5 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
                 logging.info(f"Blob name: {blob.name}")
         except Exception as e:
             logging.error(f"AZURE STORAGE ERROR: {e}")
-        # cur.execute("SELECT * FROM gh_data_item limit 3;")
-        # rows = cur.fetchall()
-        # for row in rows:
-        #     logging.info(row)
-        # cur.close()
-        # con.close()
     except Exception as e:
         logging.error(f"DATABASE CONNECT ERROR: {e}")
