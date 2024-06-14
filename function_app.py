f3f2b2a36b15b06061ce4a3023a7b41b29e92c1f
Suah-Cho <sacho0929@gmail.com>
2024-06-14 10:54:24 +0900

    add sshtunnel

--- function_app.py
+++ function_app.py
@@ -1,6 +1,7 @@
 import logging
 import azure.functions as func
 import psycopg2
+import sshtunnel
 import os
 
 app = func.FunctionApp()
@@ -12,24 +13,27 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
     logging.info('!!!!!!!!!Python timer trigger function executed.!!!!!!!!')
     logging.error('!!!!!add psycopg2!!!!!')
 
-    # SSH_HOST = os.environ['SSH_HOST']
-    # SSH_USERNAME = os.environ['SSH_USERNAME']
-    # logging.info(f"SSH_HOST: {SSH_HOST}, SSH_USERNAME: {SSH_USERNAME}")
-    db_connection_string = os.environ['DB_CONNECTION_STRING']
-    logging.info(f"DB_CONNECTION_STRING: {db_connection_string}")
-    logging.error('!!!!I WANNA SEE DB_CONNECTION_STRING!!!!')
+    SSH_HOST = os.environ['SSH_HOST']
+    SSH_USERNAME = os.environ['SSH_USERNAME']
+    SSH_KEY = os.environ['SSH_KEY']
+    logging.info(f"SSH_HOST: {SSH_HOST}, SSH_USERNAME: {SSH_USERNAME}")
+    DB_HOST = os.environ['DB_HOST']
+    DB_USER = os.environ['DB_USER']
+    DB_NAME = os.environ['DB_NAME']
+    DB_PASSWORD = os.environ['DB_PASSWORD']
+    logging.error(f'DB_HOST: {DB_HOST}, DB_USER: {DB_USER}, DB_NAME: {DB_NAME}, DB_PASSWORD: {DB_PASSWORD}')
 
     # connect to azure database
-    try:
-        con = psycopg2.connect(db_connection_string)
-        cur = con.cursor()
-        logging.info("Connected to database")
+    # try:
+        
+    #     cur = con.cursor()
+    #     logging.info("Connected to database")
 
-        cur.execute("SELECT * FROM gh_data_item limit 3;")
-        rows = cur.fetchall()
-        for row in rows:
-            logging.info(row)
-        cur.close()
-        con.close()
-    except Exception as e:
-        logging.error(f"DB CONNECT Error: {e}")
+    #     cur.execute("SELECT * FROM gh_data_item limit 3;")
+    #     rows = cur.fetchall()
+    #     for row in rows:
+    #         logging.info(row)
+    #     cur.close()
+    #     con.close()
+    # except Exception as e:
+    #     logging.error(f"DB CONNECT Error: {e}")
