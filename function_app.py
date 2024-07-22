ce941f3713ce0619de80cd3ea93eac7f6dea2b6e
SuahCho29 <suajo@croft-ai.com>
2024-07-22 11:32:37 +0900

    add event grid trigger

--- function_app.py
+++ function_app.py
@@ -114,23 +114,7 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
     except Exception as e:
         logging.error(f"DATABASE CONNECT ERROR: {e}")
 
-@app.route(route="http_trigger", auth_level=func.AuthLevel.FUNCTION)
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+@app.event_grid_trigger(arg_name="azeventgrid")
+def EventGridTrigger(azeventgrid: func.EventGridEvent):
+    logging.info('Python EventGrid trigger processed an event')
+    logging.info(f"EventGridEvent: {azeventgrid.get_json()}")
